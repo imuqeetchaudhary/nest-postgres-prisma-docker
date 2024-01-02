5f3b7ba2fa4c40691492b031b65edfda674edc75
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2024-01-02 15:10:05 +0500

    added user update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 
+import { EditUserDto } from './dto';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
+  constructor(private userService: UserService) {}
   @Get('me')
-  geMe(@GetUser() user: User): User {
+  getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
